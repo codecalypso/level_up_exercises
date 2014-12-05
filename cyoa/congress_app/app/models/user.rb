7bf1984add2c19ddeeaa2983cdc38ac2add93096
Stacy Mullins <smullins10@gmail.com>
2014-12-05 10:17:42 -0600

    Install rspec

--- cyoa/congress_app/app/models/user.rb
+++ cyoa/congress_app/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :title, length: {minimum: 3}
+  validates :email, presence: true
+  validates :email, length: {minimum: 5}
+  validates :password, presence: true
+  validates :password, length: {minimum: 5}
+  validates :email_conf, presence: true
+  validates :email_conf, length: {minimum: 5}
 end
