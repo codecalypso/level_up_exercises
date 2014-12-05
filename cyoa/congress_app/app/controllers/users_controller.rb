367becc5f8b962b455a26a139a41203504f449d3
Stacy Mullins <smullins10@gmail.com>
2014-12-05 09:17:30 -0600

    Create new rails app and controller

---
+++ cyoa/congress_app/app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+
+def index
+  @user = User.all
+end
+
+def show
+  @user = User.find(param[:id])
+end
+
+def new
+  @user = User.find(param[:id])
+end
+
+def create
+  @user= User.new
+    if @user.save
+     redirect_to(:action => 'index')
+    else
+     render('new')
+   end
+end
+
+def edit
+  @user = User.find(param[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+   if @user.update_attributes(user_params)
+    redirect_to(:action => 'show', :id => @user.id)
+  else
+    render('index')
+  end
+end
+
+def delete
+  @user = User.find(param[:id])
+end
+
+def destroy
+  User.find(params[:id]).destroy
+  redirect_to(:action => 'index')
+end
+
+private
+def user_params
+  params.require(:user).permit()
+end
