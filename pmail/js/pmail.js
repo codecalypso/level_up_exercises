9de610e3e2e4913a899cb48f57fd37df55397027
Stacy Mullins <smullins10@gmail.com>
2015-01-13 16:32:40 -0600

    Add additional changes per review log

--- pmail/js/pmail.js
+++ pmail/js/pmail.js
@@ -3,39 +3,12 @@ $(function(){
    $('.menu li').click(function(){
      $(".menu li").removeClass("float-bar");
      $(this).addClass("float-bar");
-
     });
-
-    // $('.select-button').click(function(){
-    //   $(".boxmenu").toggle();
-    // });
-
-    // $('.more-button').click(function(){
-    //   $(".morebox").toggle();
-    // });
-
-    // $('.mail-button').click(function(){
-    //   $(".mail-dropdown").toggle();
-    // });
-
-
-    // $(".settings-button").click(function(){
-    //   $('.box').toggle();
-    // });
-
-
-    // $(".app-image").click(function(){
-    //   $('.app-wrapper').toggle();
-    // });
-
-    // $(".search-dropdown-button").click(function(){
-    //   $('.container').toggle();
-    // });
 
     $(".dropdown-button").click(function(){
       $(this).find(".dropdown").toggle();
     });
-});
+  });
 
 
 
