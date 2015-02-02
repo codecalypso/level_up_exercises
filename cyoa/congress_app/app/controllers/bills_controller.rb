49026c8648ffa4890d3dfe8a2a99631783583aee
Stacy Mullins <smullins10@gmail.com>
2015-02-02 12:15:39 -0600

    Add show view

--- cyoa/congress_app/app/controllers/bills_controller.rb
+++ cyoa/congress_app/app/controllers/bills_controller.rb
@@ -2,19 +2,15 @@ class BillsController < ApplicationController
 
   def index
     Congress.key = "1dec920d92b44e9b9219e925a987267c"
-     @bills = Congress.bills
-     @results = @bills["results"].map do |result|
-     {title: result["official_title"],
-     introduced_on: result["introduced_on"]}
-   end
+    @bills = Congress.bills
+    @results = @bills["results"].map do |result|
+    {title: result["official_title"],
+    introduced_on: result["introduced_on"]}
+    end
   end
 
   def show
-  end
-
-  def params
-    @chamber = @bills["results"].map{|result|result["chamber"]}
-    @congress = @bills["results"].map{|result|result["Congress"]}
+    @bills = Congress.bills["re"]
   end
 end
 
